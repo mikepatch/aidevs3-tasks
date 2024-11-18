7a1644ceeec7fb0e1469974f9fee40d7cdc51df8
Michal Lata <michallata.dev@gmail.com>
2024-11-18 18:08:08 +0100

    feat: Change exporting type; add getData method; update _fetch method

--- src/services.ts/TasksProvider.ts
+++ src/services.ts/TasksProvider.ts
@@ -1,23 +1,31 @@
 type AnswerType = string | string[] | number | number[] | object;
 
-class TasksProvider {
+export class TasksProvider {
   protected API_KEY: string;
-  rootUrl: string;
+  private rootUrl: string;
 
   constructor() {
     this.API_KEY = import.meta.env.AIDEVS_API_KEY;
-    this.rootUrl = "https://centrala.ag3nts.org/report";
+    this.rootUrl = "https://centrala.ag3nts.org";
   }
-  async sendAnswer(taskName: string, answer: AnswerType) {
+
+  async getData(taskName: string) {
+    return this._fetch(`/data/${this.API_KEY}/${taskName}`);
+  }
+
+  async sendAnswer(
+    taskName: string,
+    answer: AnswerType
+  ): Promise<{ code: string; message: string }> {
     const options: RequestInit = {
       method: "POST",
       body: JSON.stringify({ task: taskName, apikey: this.API_KEY, answer }),
     };
 
-    return this._fetch(options, `/verify`);
+    return this._fetch("/report/verify", options);
   }
 
-  private async _fetch(options: RequestInit, additionalPath = "") {
+  private async _fetch(additionalPath = "", options?: RequestInit) {
     try {
       const url = this.rootUrl + additionalPath;
       const response = await fetch(url, options);
@@ -28,5 +36,3 @@ class TasksProvider {
     }
   }
 }
-
-export default new TasksProvider();
