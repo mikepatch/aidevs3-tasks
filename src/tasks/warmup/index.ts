0c805917329b7f5a662c0245a4d3053ba001a0af
Michal Lata <michallata.dev@gmail.com>
2024-11-24 21:51:02 +0100

    patch: Update warmup task

--- src/tasks/warmup/index.ts
+++ src/tasks/warmup/index.ts
@@ -1,12 +1,13 @@
-import TasksProvider from "../../services.ts/TasksProvider";
+import { TasksProvider } from "../../services/TasksProvider";
 
 (async () => {
+  const tasksService = new TasksProvider();
   const response = await fetch("https://poligon.aidevs.pl/dane.txt");
   const parsedResponse = await response.text();
   const responseArr = parsedResponse.trim().split("\n");
 
   console.log(responseArr);
 
-  const answerResponse = await TasksProvider.sendAnswer("POLIGON", responseArr); //TasksProvider endpoint changed so it will not work now
+  const answerResponse = await tasksService.sendAnswer("POLIGON", responseArr); //TasksProvider endpoint changed so it will not work now
   console.log(answerResponse);
 })();
